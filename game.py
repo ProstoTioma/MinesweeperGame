15e8ad7d7d96f273dacc855a2842775991a15cc7
markNevmerzhytskyi <mark.nevmerzh@gmail.com>
2023-03-03 17:59:53 +0100

    fix mines flag logic

--- game.py
+++ game.py
@@ -15,17 +15,21 @@ class Game:
             line = random.randint(0, len(self.field) - 1)
             el_ind = random.randint(0, len(self.field) - 1)
 
-            self.field[el_ind][line] = -1
+            self.field[el_ind][line] = -2
 
     def click(self, button, square_index):
         if button == 1:
-            if self.field[square_index] == -1:
+            if self.field[square_index] == -2:
+                self.field[square_index] = -1
                 self.game_over = True
                 print("Game Over!")
             elif self.field[square_index] == 0:
                 self.field[square_index] = 1
-
+        elif button == 3:
+            self.field[square_index] = 2
     def neighbors(self, a, radius, row_number, column_number):
         return [[a[i][j] if 0 <= i < len(a) and 0 <= j < len(a[0]) else 0
                  for j in range(column_number - 1 - radius, column_number + radius)]
                 for i in range(row_number - 1 - radius, row_number + radius)]
+
+
--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from game import Game
 def start():
     pygame.init()
     current_display = Screen(1000, 800)
-    print(current_display.game.field)
     current_display.draw()
-
 
 start()
