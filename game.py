b89e29e7568920f4c99ed2fac5b5761335fc6835
ProstoTioma <artemyermolenko14@gmail.com>
2023-03-09 19:25:37 +0100

    texture of flag added

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ class Game:
         self.game_over = False
         self.field = np.zeros([6, 6])
         self.generate_mines(10)
+        self.flags_number = 10
+        self.neighbors_count = 0
 
     def generate_mines(self, n):
         for i in range(n):
@@ -18,6 +20,7 @@ class Game:
             self.field[el_ind][line] = -2
 
     def click(self, button, square_index):
+        self.neighbors_count = 0
         if button == 1:
             if self.field[square_index] == -2:
                 self.field[square_index] = -1
@@ -25,11 +28,25 @@ class Game:
                 print("Game Over!")
             elif self.field[square_index] == 0:
                 self.field[square_index] = 1
+                neighbours = self.get_neighbors(self.field, square_index[0], square_index[1])
+                print(neighbours)
+                for neighbour in neighbours:
+                    if neighbour == -2:
+                        self.neighbors_count += 1
+                print(self.neighbors_count)
+
         elif button == 3:
-            self.field[square_index] = 2
-    def neighbors(self, a, radius, row_number, column_number):
-        return [[a[i][j] if 0 <= i < len(a) and 0 <= j < len(a[0]) else 0
-                 for j in range(column_number - 1 - radius, column_number + radius)]
-                for i in range(row_number - 1 - radius, row_number + radius)]
+            if self.flags_number > 0:
+                self.field[square_index] = 2
+                self.flags_number -= 1
 
+    def get_neighbors(self, matrix, x, y):
+        num_rows, num_cols = len(matrix), len(matrix[0])
+        neighbours = []
 
+        for i in range((0 if x - 1 < 0 else x - 1), (num_rows if x + 2 > num_rows else x + 2), 1):
+            for j in range((0 if y - 1 < 0 else y - 1), (num_cols if y + 2 > num_cols else y + 2), 1):
+                if matrix[x][y] != matrix[i][j]:
+                    neighbours.append(matrix[i][j])
+
+        return neighbours
--- screen.py
+++ screen.py
@@ -1,4 +1,5 @@
 import pygame
+
 from game import Game
 
 
@@ -11,24 +12,36 @@ class Screen:
         self.bg = (98, 150, 186)
         self.game = Game()
         self.sprites = []
+        self.flag_image = pygame.image.load(
+            r"C:\Users\artio\PycharmProjects\pythonLessons\src\MinesweeperGame\sprites\flag_icon.png")
+        self.flag_rect = self.flag_image.get_rect()
+
+        # # Plot Numbers
+        # font = pygame.font.SysFont('arial', 150)
+        # text = font.render(str(self.game.neighbors_count), True, (0, 0, 0))
+        # self.screen.blit(text, rect)
+        # pygame.display.update()
 
     def draw(self):
         end = False
         self.screen.fill(self.bg)
 
         while not end:
+
             self.sprites = []
             self.draw_field(self.game.field)
 
             for event in pygame.event.get():
+                self.draw_field(self.game.field)
                 # Click
                 if event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
                     clicked_sprites = [s for s in self.sprites if s.collidepoint(pos)]
                     print(clicked_sprites[0][0] // clicked_sprites[0][2],
                           clicked_sprites[0][1] // clicked_sprites[0][3])
+
                     self.game.click(event.button, (clicked_sprites[0][1] // clicked_sprites[0][3],
-                                     clicked_sprites[0][0] // clicked_sprites[0][2]))
+                                                   clicked_sprites[0][0] // clicked_sprites[0][2]))
                     print(self.game.field)
                 if event.type == pygame.QUIT:
                     end = True
@@ -45,15 +58,28 @@ class Screen:
                 self.sprites += [rect]
                 if square == -1:
                     colour = (200, 0, 0)
+                    pygame.draw.rect(self.screen, colour, rect, 0)
+                    pygame.draw.rect(self.screen, (0, 0, 0), rect, 1)
+
                 elif square == 1:
-                    colour = (0, 0, 200)
-                elif square == 2:
                     colour = (126, 132, 247)
+                    pygame.draw.rect(self.screen, colour, rect, 0)
+                    pygame.draw.rect(self.screen, (0, 0, 0), rect, 1)
+
+
+                elif square == 2:
+                    colour = (162, 209, 73)
+                    pygame.draw.rect(self.screen, colour, rect, 0)
+                    pygame.draw.rect(self.screen, (0, 0, 0), rect, 1)
+
+                    self.screen.blit(self.flag_image, rect)
+                    pygame.display.flip()
+
 
                 else:
                     colour = (162, 209, 73)
-                pygame.draw.rect(self.screen, colour, rect, 0)
-                pygame.draw.rect(self.screen, (0, 0, 0), rect, 1)
+                    pygame.draw.rect(self.screen, colour, rect, 0)
+                    pygame.draw.rect(self.screen, (0, 0, 0), rect, 1)
 
                 countSquare += 1
             countRaw += 1
